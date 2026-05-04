cd0efce9858bff403ca922e36be7cde982d8b0a9
NMC2007 <nmc281207@gmail.com>
2026-05-04 12:42:29 +0700

    update ss15

---
+++ session15/src/main/java/com/example/session15/security/userdetail/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.session15.security.userdetail;
+
+import com.example.session15.model.entity.User;
+import com.example.session15.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("not found"));
+        return new CustomUserDetails(user);
+    }
+}
